f34c997ab499d0ca37fa9aee1d64ad95aea4a290
Gopal Gautam <104998098+GOPALGTM@users.noreply.github.com>
2022-10-20 16:12:49 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,6 @@
 #!bin/bash
-sudo apt-get update
-sudo apt-get install python3-pip python3-dev libpq-dev nginx curl -y
-sudo -H pip3 install --upgrade pip
-sudo -H pip3 install virtualenv
+apt-get update
+apt-get install python3-pip python3-dev libpq-dev nginx curl -y
+pip3 install --upgrade pip
+pip3 install virtualenv
 
