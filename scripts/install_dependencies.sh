4ebf25cac09d9c74b411597f8ca19330bf97713e
Gopal Gautam <104998098+GOPALGTM@users.noreply.github.com>
2022-10-20 15:08:33 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,4 @@
 #!bin/bash
-# cd /home/ubuntu/project
-cd /opt/codedeploy-agent/deployment-root/c3881bd5-438a-42bf-9fe6-c45e3af33125/d-UJYMF5HGJ/deployment-archive/
+cd /home/ubuntu/project
 sudo pip install django gunicorn
 sudo ufw allow 8000
