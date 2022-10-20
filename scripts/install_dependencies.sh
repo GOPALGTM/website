6acf3cbdbb655af624b1f5db0e990c7b2f66c657
Gopal Gautam <104998098+GOPALGTM@users.noreply.github.com>
2022-10-20 16:08:52 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,6 @@
 #!bin/bash
-cd /home/ubuntu/project
-sudo pip install django gunicorn
-sudo ufw allow 8000
+sudo apt-get update
+sudo apt-get install python3-pip python3-dev libpq-dev nginx curl -y
+sudo -H pip3 install --upgrade pip
+sudo -H pip3 install virtualenv
+
